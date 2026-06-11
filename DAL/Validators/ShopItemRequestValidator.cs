4a69f1866b293ac6839a92c10eb83c8ad3a53c13
UTA\trant <thanhtdce180654@fpt.edu.vn>
2026-06-11 10:28:29 +0700

    ThanhTD feature shopitem validate

--- DAL/Validators/ShopItemRequestValidator.cs
+++ DAL/Validators/ShopItemRequestValidator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAL.DTO;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,21 @@ using System.Threading.Tasks;
 
 namespace DAL.Validators
 {
-    internal class ShopItemRequestValidator
+    public class ShopItemRequestValidator : AbstractValidator<ShopItemRequest>
     {
+        public ShopItemRequestValidator()
+        {
+            RuleFor(x => x.ItemId)
+                .NotEmpty()
+                .WithMessage("ItemId is required.");
+
+            RuleFor(x => x.ItemQuantity)
+                .GreaterThan(0)
+                .WithMessage("Item quantity must be greater than 0.");
+
+            RuleFor(x => x.PriceAmount)
+                .GreaterThan(0)
+                .WithMessage("Price amount must be greater than 0.");
+        }
     }
 }
